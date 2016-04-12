4e0bc3b0d38a8377a28e8d8e71112b064874217d
= <nateparro2t@gmail.com>
2016-04-12 14:25:01 -0400

    users backend

---
+++ ri-jellywatch/main.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+import users
+import json
+from google.appengine.ext import ndb
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("hey")
+
+class TestPageHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write(open('testpage.html').read())
+
+class CreateOrUpdateAccountHandler(webapp2.RequestHandler):
+    def post(self):
+        token = self.request.get('token')
+        if users.is_admin(token):
+            email = self.request.get('email')
+            password = self.request.get('password') # 'true' or 'false'
+            is_admin = self.request.get('is_admin') # 'true' or 'false'
+            is_organization = self.request.get('is_organization')
+            
+            user = users.User.with_email(email)
+            if user:
+                # update existing:
+                if password: user.pwd_hash = users.hash_pwd(password)
+                if is_admin is not None: user.is_admin = is_admin == 'true'
+                if is_organization is not None: user.is_organization = is_organization == 'true'
+                message = 'Updated user'
+                user.put()
+            else:
+                # create new:
+                if password is None or password == '' or email is None or email == '':
+                    message = "Can't have an empty password or email"
+                else:
+                    user, message = users.User.create_user(email, password, is_organization == 'true', is_admin == 'true')
+                    user.put()
+            
+            response = {"success": (user is not None), "message": message}
+        else:
+            response = {"success": False, "message": "Only admins can create new users"}
+        
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.write(json.dumps(response))
+
+class DeleteUserHandler(webapp2.RequestHandler):
+    def post(self):
+        token = self.request.get('token')
+        response = {}
+        if users.is_admin(token):
+            email = self.request.get('email')
+            user = users.User.with_email(email)
+            if user:
+                user.key.delete()
+                response = {"success": True}
+            else:
+                response = {"success": False, "message": "No user with this email found"}
+        else:
+            response = {"success": False, "message": "Only adins can delete users"}
+        
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.write(json.dumps(response))
+
+class LoginHandler(webapp2.RequestHandler):
+    def post(self):
+        email = self.request.get('email')
+        password = self.request.get('password')
+        token = users.User.create_login_token(email, password)
+        
+        response = {"success": False, "token": None, "message": None}
+        
+        if token:
+            user = users.user_for_token(token)
+            response['token'] = token
+            response['user'] = user.to_json()
+            response['success'] = True
+        else:
+            response["message"] = "No matching user found"
+        
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.write(json.dumps(response))
+
+class ListUsersHandler(webapp2.RequestHandler):
+    def get(self):
+        token = self.request.get('token')
+        response = {}
+        if users.is_admin(token):
+            response = {"users": [u.to_json() for u in users.User.query()]}
+        else:
+            response = {"message": "Only admins can see the list of users"}
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.write(json.dumps(response))
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler),
+    ('/test', TestPageHandler),
+    ('/users', ListUsersHandler),
+    ('/users/update', CreateOrUpdateAccountHandler),
+    ('/users/delete', DeleteUserHandler),
+    ('/users/login', LoginHandler)
+], debug=True)
---
+++ ri-jellywatch/users.py
@@ -0,0 +1,69 @@
+from google.appengine.ext import ndb
+import pbkdf2
+import uuid
+
+TEST_MODE = False # True
+
+def hash_pwd(pwd_text):
+    salt = "AF6FD01D57DD3DD71EAAAE2C225B1AD740FAADD575B6D68D6A3507D4DC8D9AD8"
+    return pbkdf2.pbkdf2_hex(pwd_text.encode('utf-8'), salt)    
+
+def is_admin(token):
+    if TEST_MODE and token == "!admin": return True
+    u = user_for_token(token)
+    return u is not None and u.is_admin
+
+def is_valid(token):
+    if TEST_MODE and token in ('!admin', '!user'): return True
+    u = user_for_token(token)
+    return u is not None
+
+def user_for_token(token):
+    if token == '': return None
+    tk = ndb.Key(LoginToken, token).get()
+    if tk:
+        return ndb.Key(User, tk.email).get()
+    else:
+        return None
+
+class User(ndb.Model):
+    @classmethod
+    def with_email(cls, email):
+        return ndb.Key(User, email).get()
+    
+    @classmethod
+    def create_user(cls, email, pwd, is_organization, is_admin):
+        # returns (new user or None, error)
+        user = User.get_or_insert(email)
+        if user.pwd_hash != None:
+            # this user already exists
+            return False, "A user with that email already exists."
+        user.pwd_hash = hash_pwd(pwd)
+        user.is_organization = is_organization
+        user.is_admin = is_admin
+        user.put()
+        return user, None
+    
+    @classmethod
+    def create_login_token(cls, email, pwd):
+        # returns a login token string or null
+        user = ndb.Key(User, email).get()
+        if user and user.pwd_hash == hash_pwd(pwd):
+            token = uuid.uuid4().hex
+            token = LoginToken(id=token, email=email)
+            token.put()
+            return token.key.id()
+        else:
+            return None
+    
+    pwd_hash = ndb.TextProperty()
+    created = ndb.DateTimeProperty(auto_now_add=True)
+    is_organization = ndb.BooleanProperty()
+    is_admin = ndb.BooleanProperty()
+    
+    def to_json(self):
+        return {"email": self.key.id(), "is_admin": self.is_admin, "is_organization": self.is_organization}
+
+class LoginToken(ndb.Model):
+    email = ndb.StringProperty()
+    created = ndb.DateTimeProperty(auto_now_add=True)
