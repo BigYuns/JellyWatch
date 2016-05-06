51a27d47fc5416dae91c1b388f8a158c2a750e38
= <nateparro2t@gmail.com>
2016-05-06 16:59:57 -0400

    don't reload the pins all the time

--- ri-jellywatch/public/scripts/map_generator.js
+++ ri-jellywatch/public/scripts/map_generator.js
@@ -7,6 +7,22 @@ var index_check = new Set();
 
 MapPos = null;
 
+function copyBounds(b) {
+	return new google.maps.LatLngBounds(b.getSouthWest(), b.getNorthEast());
+}
+
+function expandBounds(bounds, scale) {
+	var span = bounds.toSpan();
+	var center = bounds.getCenter();
+	var firstPointToAdd = new google.maps.LatLng({lat: center.lat() + span.lat() * scale / 2, lng: center.lng() + span.lng() * scale / 2});
+	var secondPointToAdd = new google.maps.LatLng({lat: center.lat() - span.lat() * scale / 2, lng: center.lng() - span.lng() * scale / 2});
+	return copyBounds(bounds).extend(firstPointToAdd).extend(secondPointToAdd);
+}
+
+function boundsContainsBounds(big, little) {
+	return big.equals(copyBounds(big).union(little));
+}
+
 function initMap() {
     var mapOptions = {
       zoom: 13,
@@ -14,58 +30,65 @@ function initMap() {
 	  center: {lat: 41.82696354880109, lng: -71.4032554100936}
     };//maptOptions
 	var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions); 
+	
+	var lastBoundsFetched = null;
+	
     google.maps.event.addListener(map, 'bounds_changed', function(){
-	MapPos = {lat: map.getCenter().lat(), lng: map.getCenter().lng()}
-      var lat_min = map.getBounds().getSouthWest().lat(); 
-      var lat_max = map.getBounds().getNorthEast().lat(); 
-      var lon_min = map.getBounds().getSouthWest().lng(); 
-      var lon_max = map.getBounds().getNorthEast().lng();
+		var needsReload = (!lastBoundsFetched || !boundsContainsBounds(lastBoundsFetched, map.getBounds()))
+		if (needsReload) {
+			var bounds = expandBounds(map.getBounds(), 1.5);
+			lastBoundsFetched = bounds;
+	        var lat_min = bounds.getSouthWest().lat(); 
+	        var lat_max = bounds.getNorthEast().lat(); 
+	        var lon_min = bounds.getSouthWest().lng(); 
+	        var lon_max = bounds.getNorthEast().lng();
      
-      Data.getJellyfishForMap(lat_min, lat_max, lon_min, lon_max, function(data){
-        //problem: whenever a user moves the map around, the maker is newly created. 
-        //if the data is the same, don't great a marker/ once you get the data, set the data zero. 
-        for (var i=0; i<data.length; i++){            
-          var icon = {
-            url: "http://i.giphy.com/SmYfl1HKMJjri.gif",
-            size: new google.maps.Size(20, 20),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(0, 32)
-          }; //icon
-          if(!index_check.has(i)){
-            var jellyMarker = new google.maps.Marker({
-              map: map,
-              draggable:false,
-              optimized: false, 
-			  position: {lat: data[i].geometry.x, lng: data[i].geometry.y},
-				icon: iconForJellyfish(data[i])
-              //icon: icon
-            });//jellyMarker
-			(function(sighting) {
-				jellyMarker.addListener('click', function() {
-					var content = $("<dl></dl>");
-					Object.keys(sighting.jellyfish).forEach(function(jellyType) {
-						content.append($("<dt></dt>").text(jellyType));
-						content.append($("<dd></dd>").text(sighting.jellyfish[jellyType]));
-					});
+	        Data.getJellyfishForMap(lat_min, lat_max, lon_min, lon_max, function(data){
+	          //problem: whenever a user moves the map around, the maker is newly created. 
+	          //if the data is the same, don't great a marker/ once you get the data, set the data zero. 
+	          for (var i=0; i<data.length; i++){            
+	            var icon = {
+	              url: "http://i.giphy.com/SmYfl1HKMJjri.gif",
+	              size: new google.maps.Size(20, 20),
+	              origin: new google.maps.Point(0, 0),
+	              anchor: new google.maps.Point(0, 32)
+	            }; //icon
+	            if(!index_check.has(i)){
+	              var jellyMarker = new google.maps.Marker({
+	                map: map,
+	                draggable:false,
+	                optimized: false, 
+	  			  position: {lat: data[i].geometry.x, lng: data[i].geometry.y},
+	  				icon: iconForJellyfish(data[i])
+	                //icon: icon
+	              });//jellyMarker
+	  			(function(sighting) {
+	  				jellyMarker.addListener('click', function() {
+	  					var content = $("<dl></dl>");
+	  					Object.keys(sighting.jellyfish).forEach(function(jellyType) {
+	  						content.append($("<dt></dt>").text(jellyType));
+	  						content.append($("<dd></dd>").text(sighting.jellyfish[jellyType]));
+	  					});
 					
-					content.append($("<dt></dt>").text("Date"));
-					content.append($("<dd></dd>").text(sighting.date));
+	  					content.append($("<dt></dt>").text("Date"));
+	  					content.append($("<dd></dd>").text(sighting.date));
 					
-					content.append($("<dt></dt>").text("Created by"));
-					content.append($("<dd></dd>").text(sighting.user));
-					content.find("dt").css('font-weight', 'bold');
+	  					content.append($("<dt></dt>").text("Created by"));
+	  					content.append($("<dd></dd>").text(sighting.user));
+	  					content.find("dt").css('font-weight', 'bold');
 					
-					var w = new google.maps.InfoWindow({
-						content: $(content).html()
-					})
-				    w.open(map, jellyMarker);
-				})
-			})(data[i]);
-            jellyMarker.setMap(map); 
-            index_check.add(i); 
-          }//if
-        }//for loop
-      }); //callback function
+	  					var w = new google.maps.InfoWindow({
+	  						content: $(content).html()
+	  					})
+	  				    w.open(map, jellyMarker);
+	  				})
+	  			})(data[i]);
+	              jellyMarker.setMap(map); 
+	              index_check.add(i); 
+	            }//if
+	          }//for loop
+	        }); //callback function
+		} // if needsReload
     });  //addListener
 	
   // GEOLOCATION:
